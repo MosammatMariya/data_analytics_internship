a2e77ef755a6967986c34e379ac5b47d3c9d79e1
MosammatMariya <68782157+MosammatMariya@users.noreply.github.com>
2023-12-08 16:48:11 +0600

    Add files via upload

---
+++ query5.py
@@ -0,0 +1,32 @@
+from DBconnection.dbconf import PostgresConnection
+import pandas as pd
+
+class Query5:
+    def __init__(self):
+        self.con = PostgresConnection().getConnection()
+        print("Constructor Called")
+
+    def execute(self):
+        con = self.con
+        cur = con.cursor()
+
+        div_q = """ SELECT s.division, SUM(f.total_price) FROM ecomdb_star_schema.fact_table f 
+                    join ecomdb_star_schema.store_dim s on s.store_key = f.store_key
+                    join ecomdb_star_schema.time_dim t on t.time_key = f.time_key
+                    where t.year = 2015 AND s.division = 'Barishal'
+                    group by cube(s.division)
+                    order by s.division;
+                """
+        cur.execute(div_q)
+        result = cur.fetchall()
+        pd_data = pd.DataFrame(list(result), columns=["Division", "total sales"])
+        pd_data["total sales"] = pd_data["total sales"].astype("float64")
+        pd_data = pd_data.dropna()
+        # print(pd_data)
+        return pd_data.to_dict(orient='records')
+        # return result
+
+if __name__ == '__main__':
+    query5 = Query5()
+    data = query5.execute()
+    print(data)
