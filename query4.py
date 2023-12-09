b8621691940cf809176e4635685d015ed30c02ce
MosammatMariya <68782157+MosammatMariya@users.noreply.github.com>
2023-12-10 00:15:54 +0600

    query4.py

--- query4.py
+++ query4.py
@@ -6,27 +6,25 @@ class Query4:
         self.con = PostgresConnection().getConnection()
         print("Constructor Called")
 
+    @property
     def execute(self):
         con = self.con
         cur = con.cursor()
 
-        div_q = """ SELECT t.year, SUM(f.total_price) FROM ecomdb_star_schema.fact_table f 
+        df_q1 = """ SELECT t.year, t.quarter, SUM(f.total_price) FROM ecomdb_star_schema.fact_table f 
                     join ecomdb_star_schema.time_dim t on t.time_key = f.time_key
                     where t.year = 2015
-                    group by cube(t.year)
-                    order by t.year;
+                    group by cube(t.year, t.quarter)
+                    order by t.quarter;
                 """
-
-        cur.execute(div_q)
-        result = cur.fetchall()
-        pd_data = pd.DataFrame(list(result), columns=["Year", "total sales"])
-        pd_data["total sales"] = pd_data["total sales"].astype("float64")
-        pd_data = pd_data.dropna()
+        cur.execute(df_q1)
+        records = cur.fetchall()
+        df_q2 = pd.DataFrame(records, columns=["year", "quarter", "total sales"])
+        df_q2 = df_q2.dropna()
         # print(pd_data)
-        return pd_data.to_dict(orient='records')
+        return df_q2.to_dict(orient='records')
         # return result
-
 if __name__ == '__main__':
     query4 = Query4()
-    data = query4.execute()
+    data = query4.execute
     print(data)
