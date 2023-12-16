e626dd5a859f39f035d71cb18f44e2215da73412
MosammatMariya <68782157+MosammatMariya@users.noreply.github.com>
2023-12-17 00:39:57 +0600

    app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,16 @@ import {Query7Component} from "./components/query7/query7.component";
 import {Query8Component} from "./components/query8/query8.component";
 import {Query9Component} from "./components/query9/query9.component";
 import {Query10Component} from "./components/query10/query10.component";
+import {Analytics1aComponent} from "./components/analytics1a/analytics1a.component";
+import {Analytics1bComponent} from "./components/analytics1b/analytics1b.component";
+import {Analytics2aComponent} from "./components/analytics2a/analytics2a.component";
+import {Analytics2bComponent} from "./components/analytics2b/analytics2b.component";
+import {Analytics3aComponent} from "./components/analytics3a/analytics3a.component";
+import {Analytics3bComponent} from "./components/analytics3b/analytics3b.component";
+import {Analytics4aComponent} from "./components/analytics4a/analytics4a.component";
+import {Analytics4bComponent} from "./components/analytics4b/analytics4b.component";
+import {Analytics5aComponent} from "./components/analytics5a/analytics5a.component";
+
 
 const routes: Routes = [
   {path: 'first-component', component: AddTutorialComponent},
@@ -26,6 +36,15 @@ const routes: Routes = [
   {path: '8', component: Query8Component},
   {path: '9', component: Query9Component},
   {path: '10', component: Query10Component},
+  {path: 'a1a', component:Analytics1aComponent},
+  {path: 'a1b',component: Analytics1bComponent},
+  {path: 'a2a',component: Analytics2aComponent},
+  {path: 'a2b',component: Analytics2bComponent},
+  {path: 'a3a',component: Analytics3aComponent},
+  {path:'a3b',component: Analytics3bComponent},
+  {path:'a4a',component: Analytics4aComponent},
+  {path:'a4b',component: Analytics4bComponent},
+  {path:'a5a',component: Analytics5aComponent}
 ];
 
 @NgModule({
